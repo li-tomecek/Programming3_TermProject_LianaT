885db7d9c199b0fae494632a66e941cd9f35ffe3
Liana Tomecek <liana.tomecek@gmail.com>
2025-11-04 21:32:07 -0700

    Got rid of old comments

--- Assets/_Scripts/Cards/CardTypes/ForceRotationCard.cs
+++ Assets/_Scripts/Cards/CardTypes/ForceRotationCard.cs
@@ -8,8 +8,6 @@ public class ForceRotationCard : Card
     public override void Play(Disk castingDisk)
     {
         base.Play(castingDisk);
-        //Will have to figure out how to treat player vs enemy disks. Players rotate the disks before the resolution phase which makes this tricky.
-        //Maybe instead of force left/right/no movement, it forces it to a specific spell type.
 
         TargetDisk.RotateByPosition(_position);
         TargetDisk.LockRotation();
