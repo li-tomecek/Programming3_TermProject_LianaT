65096e9e2b523d73ea58ade316f8801ebe2588ee
Liana Tomecek <60552485+li-tomecek@users.noreply.github.com>
2025-11-26 18:11:06 -0700

    added skeleton for game state manager
    this will be for paused, unpaused, submenus, etc

--- Assets/_Scripts/States/GameStateManager.cs
+++ Assets/_Scripts/States/GameStateManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Linq;
 using UnityEngine;
 
-public class BattleStateManager : Singleton<BattleStateManager>
+public class GameStateManager : Singleton<GameStateManager>
 {
     private IState _currentState;
 
@@ -14,33 +14,6 @@ public class BattleStateManager : Singleton<BattleStateManager>
     public PreparationPhase PreparationPhase => _prepPhase;
     public ResolutionPhase ResolutionPhase => _resolutionPhase;
 
-
-    void Start()
-    {
-        StartCoroutine(Setup());    //need to add a delay to make sure everyone's start functions have happened
-    }
-
-    private IEnumerator Setup()
-    {
-        PreparationPhase.Exit();    //set buttons and disks to inactive temporarily
-
-        //Rotate each disk randomly to start
-        int index;
-        foreach (Disk disk in Player.Instance.GetDisks().Concat(Opponent.Instance.GetDisks()))
-        {
-            index = Random.Range(0, disk.GetSpellList().Length);
-            disk.SetActiveSpell(disk.GetSpellList()[index]);            //choose a random spell to rotate to
-        }
-
-        RoutineSequencer.Instance.AddSimultaneous(
-            Opponent.Instance.RotateDisksToSelected(),
-            Player.Instance.RotateDisksToSelected());
-        
-        yield return RoutineSequencer.Instance.ActiveRoutine;
-        ChangeState(_prepPhase);
-    }
-
-    // ----------------------------------------------------
     
     void Update()
     {
